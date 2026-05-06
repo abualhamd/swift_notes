e85589ef51bd4baa4b3f6880f0a810e401413801
abualhamd <53705516+abualhamd@users.noreply.github.com>
2026-05-06 03:12:40 +0300

    feat: collections

--- Sources/first_project/first_project.swift
+++ Sources/first_project/first_project.swift
@@ -1,7 +1,7 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-extension Collection {
+extension Array {
     subscript(safe index: Index) -> Element? {
         return indices.contains(index) ? self[index] : nil
     }
@@ -10,14 +10,18 @@ extension Collection {
 @main
 struct first_project {
     static func main() {
-        // Optional Binding
-        var t = Double("xx")
-        if let sanitaizedT = t  {
-            print("The value of t is: \(sanitaizedT)")
-        } else {
-            print("Could not convert string to Double.")
-        }
-        var t2 = Double("8")
-        if let t2 {print ("The value of t2 is: \(t2)")} else {print("Could not convert string to Double.")}
+        // collections
+        var arr = [1, 2, 3, 4, 5, 5]
+        var set1 = Set(arr)
+        var set2: Set = ["a", "b", "c"]
+        var dect = ["one": 1, "two": 2, "three": 3]
+
+        print(arr[safe: 8] ?? 0) // This will return nil, as index 8 is out of bounds for the array
+        print(dect["four"]) // This will return nil, as "four" is not a key in the dictionary
+
+        // let emptyArr:[Any] = []
+        let emptyArr = [Any]()
+        // let emptyDict = [String: Any]()
+        let emptyDict:[String:Any] = [:]
     }
 }
